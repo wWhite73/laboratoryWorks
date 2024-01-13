f121200fed3c63c74ad05f1658a4593fec6cfb5a
White73 <118853384+wWhite73@users.noreply.github.com>
2024-01-13 22:58:58 +0300

    Add files via upload

---
+++ КПО/lab1/lab1/lab1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+int factoriall(int x) {
+	int result = 1;
+	for (int i = 1; i <= x; i++) {
+		result *= i;
+	}
+
+	return result;
+}
+
+class  sinIs {
+	double element = 1;
+	double sum = this->element;
+
+	double calcElement(double x, int i) {
+		return pow(-1, i)* pow(x, 2 * i + 1) / factoriall(2 * i + 1);
+	}
+
+public:
+	sinIs(long double x, int n) {
+		for (int i = 1; i < n; i++) {
+			/*this->sum += calcElement(x, i);*/
+			this->element *= x / i;
+			sum += element;
+		}
+	}
+
+	void print_sum() {
+		cout << "Сумма: "<< this->sum<< endl;
+	}
+
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	cout << "Введите количество членов суммы: ";
+	cin >> n;
+
+	long double x;
+	cout << "Введите значение x: ";
+	cin >> x;
+
+	sinIs value = sinIs(x, n);
+	value.print_sum();
+
+	return 0;
+}
